1e34a899cf861a8efcd27e00c4590a40a38758af
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-05-20 22:09:45 +0200

    Updating file names

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import './assets/css/navalgroup.css';
-import App from './App.jsx';
+import App from './App';
 ReactDOM.render(<App/>,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
