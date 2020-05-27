5742eddea9e3944436f7e8559076e6bc46f25f44
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-05-27 23:55:27 +0200

    End of Regular Forms View

--- src/views/RegularFormView/RegularFormView.js
+++ src/views/RegularFormView/RegularFormView.js
@@ -4,10 +4,12 @@ import {
     Grid,
     Card, CardHeader, CardContent, Avatar,
     TextField, Checkbox, Button,
-    FormControlLabel, Box, Typography
+    FormControlLabel, Box, Typography,
+    Radio, RadioGroup, FormGroup
 } from '@material-ui/core';
 import {
-    AccessibilityNewSharp as AccessibilityNewSharpIcon, 
+    AccessibilityNewSharp as AccessibilityNewSharpIcon,
+    ListAlt as ListAltIcon
 } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
@@ -30,7 +32,12 @@ const useStyles = makeStyles(theme => ({
         }
     },
     horizontalItem: {
-        paddingTop: theme.spacing(3)
+        paddingTop: theme.spacing(2),
+    },
+    horizontalItemLabel: {
+        paddingTop: theme.spacing(3),
+        textAlign: "right",
+        paddingRight: theme.spacing(3)
     }
 }));
 
@@ -41,8 +48,8 @@ const RegularFormView = (props) => {
             <Grid item md={6} sm={12}>
                 <Card className={classes.cardroot}>
                     <CardHeader title={
-                            <Typography variant="h4">Stacked Form</Typography>
-                        }
+                        <Typography variant="h4">Stacked Form</Typography>
+                    }
                         avatar={
                             <Avatar aria-label="recipe" className={classes.avatar}>
                                 <AccessibilityNewSharpIcon />
@@ -51,13 +58,13 @@ const RegularFormView = (props) => {
                     <CardContent>
                         <form className={classes.formroot} noValidate autoComplete="off">
                             <Box component="span" display="block">
-                                <TextField label="Email adress" fullWidth="true"/>
+                                <TextField label="Email adress" fullWidth="true" />
                             </Box>
                             <Box component="span" display="block">
-                                <TextField label="Password" type="password" fullWidth="true"/>
+                                <TextField label="Password" type="password" fullWidth="true" />
                             </Box>
                             <Box component="span" display="block">
-                                <FormControlLabel control={<Checkbox color="primary"/>} label="Subscribe to newsletter"/>
+                                <FormControlLabel control={<Checkbox color="primary" />} label="Subscribe to newsletter" />
                             </Box>
                             <Box component="span" display="block">
                                 <Button variant="contained" color="primary">Submit</Button>
@@ -69,8 +76,8 @@ const RegularFormView = (props) => {
             <Grid item md={6} sm={12}>
                 <Card className={classes.cardroot}>
                     <CardHeader title={
-                            <Typography variant="h4">Horizontal Form</Typography>
-                        }
+                        <Typography variant="h4">Horizontal Form</Typography>
+                    }
                         avatar={
                             <Avatar aria-label="recipe" className={classes.avatar}>
                                 <AccessibilityNewSharpIcon />
@@ -79,29 +86,29 @@ const RegularFormView = (props) => {
                     <CardContent>
                         <form noValidate autoComplete="off">
                             <Grid container>
-                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem}>
+                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItemLabel}>
                                     <Typography color="textSecondary">Email adress</Typography>
                                 </Grid>
                                 <Grid item md={9} sm={12} xs={12} className={classes.horizontalItem}>
                                     <Box component="span" display="block">
-                                        <TextField fullWidth="true"/>
+                                        <TextField fullWidth="true" />
                                     </Box>
                                 </Grid>
-                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem}>
+                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItemLabel}>
                                     <Typography color="textSecondary">Password</Typography>
                                 </Grid>
                                 <Grid item md={9} sm={12} xs={12} className={classes.horizontalItem}>
                                     <Box component="span" display="block">
-                                        <TextField type="password" fullWidth="true"/>
+                                        <TextField type="password" fullWidth="true" />
                                     </Box>
                                 </Grid>
-                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem}/>
+                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem} />
                                 <Grid item md={9} sm={12} xs={12} className={classes.horizontalItem}>
                                     <Box component="span" display="block">
-                                        <FormControlLabel control={<Checkbox color="primary"/>} label="Subscribe to newsletter"/>
+                                        <FormControlLabel control={<Checkbox color="primary" />} label="Subscribe to newsletter" />
                                     </Box>
                                 </Grid>
-                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem}/>
+                                <Grid item md={3} sm={12} xs={12} className={classes.horizontalItem} />
                                 <Grid item md={9} sm={12} xs={12} className={classes.horizontalItem}>
                                     <Box component="span" display="block">
                                         <Button variant="contained" color="primary">Submit</Button>
@@ -111,7 +118,89 @@ const RegularFormView = (props) => {
                         </form>
                     </CardContent>
                 </Card>
-            </Grid>        
+            </Grid>
+            <Grid container className={classes.root}>
+                <Grid item sm={12}>
+                    <Card className={classes.cardroot}>
+                        <CardHeader title={
+                            <Typography variant="h4">Form Elements</Typography>
+                        }
+                            avatar={
+                                <Avatar aria-label="recipe" className={classes.avatar}>
+                                    <ListAltIcon />
+                                </Avatar>
+                            } />
+                        <CardContent>
+                            <form noValidate autoComplete="off">
+                                <Grid container>
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">With Help</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <Box component="span" display="block">
+                                            <TextField fullWidth="true" />
+                                        </Box>
+                                    </Grid>
+
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">Password</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <Box component="span" display="block">
+                                            <TextField fullWidth="true" type="password" />
+                                        </Box>
+                                    </Grid>
+
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">Placeholder</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <Box component="span" display="block">
+                                            <TextField fullWidth="true" placeholder="placeholder" />
+                                        </Box>
+                                    </Grid>
+
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">Disabled</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <Box component="span" display="block">
+                                            <TextField fullWidth="true" disabled placeholder="disabled" />
+                                        </Box>
+                                    </Grid>
+
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">Checkboxes and radios</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <Box component="span" display="block">
+                                            <FormControlLabel control={<Checkbox color="primary" />} label="First Checkbox" />
+                                        </Box>
+                                        <Box component="span" display="block">
+                                            <FormControlLabel control={<Checkbox color="primary" />} label="Second Checkbox" />
+                                        </Box>
+                                        <RadioGroup aria-label="gender" name="gender1">
+                                            <FormControlLabel value="FirstValue" control={<Radio color="primary"/>} label="First Radio" />
+                                            <FormControlLabel value="SecondValue" control={<Radio color="primary" />} label="Second Radio" />
+                                        </RadioGroup>
+                                    </Grid>
+                                    <Grid item md={2} sm={12} xs={12} className={classes.horizontalItemLabel}>
+                                        <Typography color="textSecondary">Inline Checkboxes</Typography>
+                                    </Grid>
+                                    <Grid item md={10} sm={12} xs={12} className={classes.horizontalItem}>
+                                        <FormGroup row>
+                                            <FormControlLabel control={<Checkbox color="primary" />} label="a" />
+                                            <FormControlLabel control={<Checkbox color="primary" />} label="b" />
+                                            <FormControlLabel control={<Checkbox color="primary" />} label="c" />
+                                        </FormGroup>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+            </Grid>
         </Grid>
     );
 };
